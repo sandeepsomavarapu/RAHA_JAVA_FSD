6a64572459e5c076a191ab940b60c419031830a9
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-05-02 20:06:17 +0530

    02-05-2024

---
+++ ArraysEx.java
@@ -0,0 +1,43 @@
+
+public class ArraysEx {
+
+	public static void main(String[] args) {
+		
+		int arr[][]= {{12,32,11},{22,54,43},{90,21,33}};
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		int ids[]= {12,43,14,22,54,33,22,11};
+//		
+//		
+//		for(int i:ids)
+//		{
+//			System.out.println(i);
+//		}
+		
+		
+		
+//		for(int i=0;i<ids.length;i++)
+//		{
+//			System.out.println(ids[i]);
+//		}
+
+	}
+
+}
